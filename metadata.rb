21c45cf526ea88e18eabf38b4378704d8b60c582
Robert Ferney <rob@ferney.org>
2018-08-06 18:05:49 -0600

    update

--- metadata.rb
+++ metadata.rb
@@ -15,6 +15,7 @@ depends 'java', '~> 2.1.0'
 depends 'git', '~> 9.0.1'
 depends 'postgresql', '~> 7.0.0'
 depends 'nodejs', '~> 5.0.0'
+# depends 'rvm', '~> 1.1.1'
 
 # depends 'gpg', '~> 0.3.0'
 # depends 'rvm', '~> 0.9.4'
