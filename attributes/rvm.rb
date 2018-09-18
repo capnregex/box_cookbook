4c231e6b0683c23b6711d9d04b1665ebe1a509a5
Robert W Ferney <robert.w.ferney@uscis.dhs.gov>
2018-09-18 16:55:06 +0000

    symbol to string

--- attributes/rvm.rb
+++ attributes/rvm.rb
@@ -3,5 +3,5 @@ default[:rvm][:git][:version] = 'stable'
 default[:rvm][:rubies] = %w(ruby-2.4.4 ruby-2.5.1)
 default[:rvm][:use] = "ruby-2.4.4"
 default[:rvm][:user][:vagrant] = {
-  group: :vagrant
+  group: 'vagrant'
 }
