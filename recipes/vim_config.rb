4ef7e29c509d103314405631c83f0251d08abbb5
Robert W Ferney <robert.w.ferney@uscis.dhs.gov>
2018-09-19 00:22:51 +0000

    use etc/vim/..rc.local

--- recipes/vim_config.rb
+++ recipes/vim_config.rb
@@ -1,13 +1,3 @@
-cookbook_file "/etc/vimrc" do
-  source 'etc/vimrc'
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
-
-cookbook_file "/etc/gvimrc" do
-  source 'etc/vimrc'
-  owner 'root'
-  group 'root'
-  mode '0755'
+remote_directory "/etc/vim" do
+  source 'etc/vim'
 end
