183ffd7eddd2c155e41f2026d0053f100fa00362
Robert W Ferney <robert.w.ferney@uscis.dhs.gov>
2018-12-20 23:22:17 +0000

    setup mailcatcher as a system daemon

--- recipes/mailcatcher.rb
+++ recipes/mailcatcher.rb
@@ -1,3 +1,21 @@
+
 package 'ruby-dev'
+
 gem_package 'mailcatcher'
-rvm_gem 'mailcatcher'
+
+user 'mailcatcher' do
+  system true
+end
+
+cookbook_file '/etc/init.d/mailcatcher' do
+  source 'etc/init.d/mailcatcher'
+  mode '0755'
+end
+
+service 'mailcatcher' do
+  action :enable
+end
+
+service 'mailcatcher' do
+  action :start
+end
