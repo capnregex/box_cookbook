d01af60167c60e516818558c5b63a25a83534b87
Robert W Ferney <robert.w.ferney@uscis.dhs.gov>
2018-12-20 17:18:22 +0000

    pulling docker_compose out to a seperate cookbook

--- recipes/docker-compose.rb
+++ recipes/docker-compose.rb
@@ -1,17 +1,3 @@
 
-require 'open-uri'
-
-package 'docker-compose' do
-  action :remove
-end
-
-latest = JSON.parse(open("https://api.github.com/repos/docker/compose/releases/latest").read)
-assets = latest['assets']
-asset = assets.select{|a| a['name'] == 'docker-compose-Linux-x86_64' }.first
-dl_url = asset['browser_download_url']
-
-remote_file "/usr/local/bin/docker-compose" do
-  source dl_url
-  mode 0755
-end
+include_recipe "docker_compose"
 
